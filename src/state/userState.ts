82ae57087254bad05d8aa99016c643bbc5214c35
iamajraj <akmalraj07@gmail.com>
2023-04-23 18:08:55 +0600

    postinstall added to package.json for the prisma

--- src/state/userState.ts
+++ src/state/userState.ts
@@ -15,7 +15,7 @@ const initialValue: TUser = {
 };
 
 const userState = atom<TUser>({
-  key: "user", // unique ID (with respect to other atoms/selectors)
+  key: "userState", // unique ID (with respect to other atoms/selectors)
   default: initialValue, // default value (aka initial value)
 });
 
